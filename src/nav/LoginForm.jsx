63a12034b6ff00d35c0d5d8b5358e9e924999ea1
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-19 11:01:09 +0530

    e

--- src/nav/LoginForm.jsx
+++ src/nav/LoginForm.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState} from 'react'
 
 const LoginForm = () => {
+  let [tc, setTc] = useState(true)
+
+
+
   return (
     <div>
+      <form>
+        <input type="text" placeholder='Username' />
+        <input type="password" placeholder='Password' />
+        <input type="checkbox" onClick={tchandle} />
+        <button type='submit' disabled={tc}>Login</button>
+
+    </form>
       
     </div>
   )
